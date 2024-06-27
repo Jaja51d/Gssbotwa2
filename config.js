d2b8e234b2690dfcbaa44aa749f7e3bf42e00860
Jaja51d <162837408+Jaja51d@users.noreply.github.com>
2024-06-27 18:32:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "te pup";
-global.owner = ['40726022975];
-global.premium = ['40726022975];
+global.ownername = "bot top";
+global.owner = ['0726022975];
+global.premium = ['0726022975];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
